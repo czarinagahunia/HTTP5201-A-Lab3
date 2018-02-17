ccbef97cb035f7dd9c207880e7cbd375bde28c51
Czarina <czarinagahunia@gmail.com>
2018-02-16 19:39:56 -0500

    Modified .js file. test__checkHumbrId function fixed.

--- Lab-3-unittest.js
+++ Lab-3-unittest.js
@@ -5,9 +5,9 @@
  * Returns true if input validates.
  * @param {string} idIn
  */
-var flag = false;
 function checkHumbrId(idIn) {
     "use strict";
+    var flag = false;
     var pattern = /^(n|N)\d{8}$/;
     if (!(pattern.test(idIn))) {
         flag = false;
@@ -25,28 +25,28 @@ function test__checkHumbrId(valueIn, expected) {
     var resultPass = "==PASSED==";
     var resultFail = "xxFAILEDxx";
     var outPut;
-    if (flag === true) {
-        outPut = "Student ID: " + valueIn + " | Expected result: " + expected + " | Result: " + "<span style='color:green'>" + resultPass + "</span>" + "<br />";
+    if (result === expected) {
+        outPut = "Value tested: " + valueIn + " | Expected result: " + expected + "<span style='color:green'> " + resultPass + "</span>" + "<br />";
         dataIn.innerHTML += outPut;
     } else {
-        outPut = "Student ID: " + valueIn + " | Expected result: " + expected + " | Result: " + "<span style='color:red'>" + resultFail + "</span>" + "<br />";
+        outPut = "Value tested: " + valueIn + " | Expected result: " + expected + "<span style='color:red'> " + resultFail + "</span>" + "<br />";
         dataIn.innerHTML += outPut;
     }
 }//end of test__checkHumbrId
 
-test__checkHumbrId("N01261508", true); //passed
-test__checkHumbrId("n01261508", true); //passed
-test__checkHumbrId("n0126150", false); //passed
-test__checkHumbrId("N0126150", false); //passed
-test__checkHumbrId("N012615089", false); //passed
-test__checkHumbrId("M01261508", false); //passed
-test__checkHumbrId(1234, false); //passed
-test__checkHumbrId(12345678, false); //passed
-test__checkHumbrId(123456789, false); //passed
-test__checkHumbrId(1234567890, false); //passed
-test__checkHumbrId("abcdefgh", false); //passed
-test__checkHumbrId("abcdef", false); //passed
-//test__checkHumbrId(n01261508, false); //throws error
-test__checkHumbrId(1, false); //passed
-test__checkHumbrId("", false); //passed
-test__checkHumbrId(null, false); //passed
+//Testing the function
+test__checkHumbrId("N01261508", true);
+test__checkHumbrId("n01261508", true);
+test__checkHumbrId("n0126150", false);
+test__checkHumbrId("N0126150", false);
+test__checkHumbrId("N012615089", false);
+test__checkHumbrId("M01261508", false);
+test__checkHumbrId(1234, false);
+test__checkHumbrId(12345678, false);
+test__checkHumbrId(123456789, false);
+test__checkHumbrId(1234567890, false);
+test__checkHumbrId("abcdefgh", false);
+test__checkHumbrId("abcdef", false);
+test__checkHumbrId(1, false);
+test__checkHumbrId("", false);
+test__checkHumbrId(null, false);
